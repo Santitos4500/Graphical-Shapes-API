f3de87813b2abec9c8c5c5ff24fff7934e956e5f
Anshul Shanker <ashanker@luc.edu>
2020-10-16 17:59:39 -0500

    Fix typo, complete count and pass tests

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Count.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Count.java
@@ -1,4 +1,5 @@
 package edu.luc.etl.cs313.android.shapes.model;
+import java.util.Iterator;
 
 /**
  * A visitor to compute the number of basic shapes in a (possibly complex)
@@ -6,45 +7,51 @@ package edu.luc.etl.cs313.android.shapes.model;
  */
 public class Count implements Visitor<Integer> {
 
-	// TODO entirely your job
 
 	@Override
 	public Integer onPolygon(final Polygon p) {
-		return -1;
+		return 1;
 	}
 
 	@Override
 	public Integer onCircle(final Circle c) {
-		return -1;
+		return 1;
 	}
 
 	@Override
-	public Integer onGroup(final Group g) {
-		return -1;
+	public Integer onGroup(final Group g)
+	{
+		int count = 0;
+		final Iterator<? extends Shape> shape = g.getShapes().iterator();
+
+		while(shape.hasNext()){
+			count = count + shape.next().accept(this);
+		}
+		return count;
 	}
 
 	@Override
 	public Integer onRectangle(final Rectangle q) {
-		return -1;
+		return 1;
 	}
 
 	@Override
 	public Integer onOutline(final Outline o) {
-		return -1;
+		return o.getShape().accept(this);
 	}
 
 	@Override
 	public Integer onFill(final Fill c) {
-		return -1;
+		return c.getShape().accept(this);
 	}
 
 	@Override
 	public Integer onLocation(final Location l) {
-		return -1;
+		return l.getShape().accept(this);
 	}
 
 	@Override
 	public Integer onStrokeColor(final StrokeColor c) {
-		return -1;
+		return c.getShape().accept(this);
 	}
 }
