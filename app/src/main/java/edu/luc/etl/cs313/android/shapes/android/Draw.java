b57e9e1f9dd73b31559f75828c41fd4f2f355e17
Anshul Shanker <ashanker@luc.edu>
2020-10-16 17:14:59 -0500

    onOutline and onRectangle

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -1,5 +1,6 @@
 package edu.luc.etl.cs313.android.shapes.android;
-
+// List imported for OnPolygon
+import java.util.List;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
@@ -17,8 +18,8 @@ public class Draw implements Visitor<Void> {
 	private final Paint paint;
 
 	public Draw(final Canvas canvas, final Paint paint) {
-		this.canvas = null; // FIXME
-		this.paint = null; // FIXME
+		this.canvas = canvas;
+		this.paint = paint;
 		paint.setStyle(Style.STROKE);
 	}
 
@@ -30,51 +31,56 @@ public class Draw implements Visitor<Void> {
 
 	@Override
 	public Void onStrokeColor(final StrokeColor c) {
-
+		final int newColor = paint.getColor();
+		paint.setColor(c.getColor());
+		c.getShape().accept(this);
+		paint.setColor(newColor);
 		return null;
 	}
 
 	@Override
 	public Void onFill(final Fill f) {
 
+		paint.setStyle(Style.FILL_AND_STROKE);
+		f.getShape().accept(this);
+		paint.setStyle(Style.STROKE);
 		return null;
 	}
 
 	@Override
 	public Void onGroup(final Group g) {
-		final Iterator<> extends Shape > shape = g.getShapes().iterator();
-		while (shape.hasNext()) {
-			shape.next().accept(this);
-		}
-		return null;
-
+      return null;
 	}
 
 	@Override
 	public Void onLocation(final Location l) {
-		canvas.translate(l.getX(), l.getY());
-		l.getShape().accept(this);
-		canvas.translate(-l.getX(), -l.getY());
+
 		return null;
 	}
 
 	@Override
 	public Void onRectangle(final Rectangle r) {
-
+//		https://developer.android.com/reference/android/graphics/Rect
+		canvas.drawRect(0, 0, r.getWidth(), r.getHeight(), paint);
 		return null;
 	}
 
 	@Override
 	public Void onOutline(Outline o) {
-
+        final Style ol = paint.getStyle();
+        paint.setStyle(Style.STROKE);
+        o.getShape().accept(this);
+        paint.setStyle(ol);
 		return null;
 	}
 
 	@Override
 	public Void onPolygon(final Polygon s) {
+        List<? extends Point> points = s.getPoints();
 
-		final float[] pts = null;
-
+        final float[] pts = {
+		};
+//		https://developer.android.com/reference/android/graphics/Canvas#drawLines(float%5B%5D,%2520int,%2520int,%2520android.graphics.Paint)
 		canvas.drawLines(pts, paint);
 		return null;
 	}
